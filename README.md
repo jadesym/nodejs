4af8f30a26fba3fdc038b1f1b2c8e547276444e7
Kevin Fu <kevin.fu808@gmail.com>
2015-04-21 13:57:32 -0500

    working login

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # nodejs
+mongod --> sets up mongo database
+express appname
+npm install npm -g
+npm start
+
+Have to make data directory
+mkdir data
+
+
+if npm install bcrypt doesnt work -- saves this in package.json
+sudo npm install bcrypt --save
+
+to delete everything in the database -- MONGOD needs to be running
+mongo
+show dbs  (shows all databases)
+use examplemongo
+db
+show collections
+db.users.remove({}) -- removes all users
+
+Homework: 4/20/15
+Sessions + cookies to keep you logged in
+login/logout
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/examplemongo';
+// Load the bcrypt module
+var bcrypt = require('bcrypt');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,6 +17,31 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/login', function(req, res, next){
+  MongoClient.connect(url, function(err, db) {
+    var formObjects = req.body;
+    var password = formObjects.password;
+    var input_email = formObjects.email;
+    var collection = db.collection('users');
+    collection.findOne({email: input_email}, function(err, result) {
+      if (!err) {
+        var hash = result.password;
+        if (bcrypt.compareSync(password, hash)) {
+          res.send("Successful Login");
+        } else {
+          res.send("The password is incorrect!");
+        }
+      } else {
+        res.send("That user account does not exist.");
+      }
+    });
+  });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('users/login');
+});
+
 router.get('/new', function(req, res, next) {
   res.render('users/new');
 });
@@ -22,7 +49,16 @@ router.get('/new', function(req, res, next) {
 router.post('/create', function(req, res, next) {
   MongoClient.connect(url, function(err, db) {
     var collection = db.collection('users');
-    collection.insert(req.body, function(err, result) {
+
+    // Generate a salt
+    var salt = bcrypt.genSaltSync(10);
+    // Hash the password with the salt
+    var hash = bcrypt.hashSync(req.body.password, salt);
+    // var hash = bcrypt.hashSync("my password", 10); Can be done in one line
+    var formObjects = req.body;
+    var password = formObjects.password;
+    formObjects.password = hash;
+    collection.insert(formObjects, function(err, result) {
       if (!err) {
         res.send("User created");
       } else {
@@ -31,4 +67,6 @@ router.post('/create', function(req, res, next) {
     });
   });
 });
+
+
 module.exports = router;
